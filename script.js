13df4fdbc6b8b1568049f407337879152aba18fc
MyceliumSpawning <github.com.astrology922@passinbox.com>
2024-10-15 14:25:48 +0800

    Create calculator to evaluate basic expressions

---
+++ script.js
@@ -0,0 +1,68 @@
+// Creates basic calculator operations
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let num1 = "foo";
+let num2 = "bar";
+let op = 0;
+let storage = [];
+
+function operator(num1, num2, op) {
+    if (op === "+") {
+        return add(num1, num2);
+    } else if (op === "-") {
+        return subtract(num1, num2);
+    } else if (op === "*") {
+        return multiply(num1, num2);
+    } else if (num2 == 0 && op === "/") {
+        return "haha go away"; 
+    } else if (op === "/") {
+        return divide(num1, num2);
+    } 
+}
+
+ // Makes buttons and display interactive
+const buttons = document.querySelector(".buttons");
+const display = document.querySelector(".display");
+
+const span = document.createElement("span");
+
+buttons.addEventListener("click", event => {
+    let target = event.target;
+    if (target.matches("button")) {
+        let value = target.innerHTML;
+        span.textContent += value;
+        display.appendChild(span);
+        storage.push(value);
+        console.log(storage);
+        }
+
+    if (target.matches("#equals")) {
+        let newStorage = storage.join('').split(/([_\W])/);
+        console.log(newStorage);
+        let result = operator(parseInt(newStorage[0]), parseInt(newStorage[2]), newStorage[1]);
+        span.textContent += result;
+        display.appendChild(span);
+    }
+
+    if (target.matches("#clear")) {
+        span.textContent = "";
+        display.replaceChildren(span);
+        storage = [];
+        console.log(storage);
+    }
+})
+
